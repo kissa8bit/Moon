05d04f6d53af9eb9d9d1abc9d339fbc0496ec6cb
kirill <kirill_ii@mail.ru>
2024-10-12 12:58:21 +0200

    move vulkan_tools

--- scripts/shaders_compile.bat
+++ scripts/shaders_compile.bat
@@ -1,7 +1,7 @@
 set dir_path=%~dp0
 
-python3 %dir_path%/shaders_compile.py -d %dir_path%/../core/deferredGraphics/shaders -o %dir_path%/../core/deferredGraphics/spv -c %dir_path%/../dependences/libs/vulkan_tools/glslc.exe
+python3 %dir_path%/shaders_compile.py -d %dir_path%/../core/deferredGraphics/shaders -o %dir_path%/../core/deferredGraphics/spv -c %dir_path%/../dependences/vulkan_tools/glslc.exe
 
-python3 %dir_path%/shaders_compile.py -d %dir_path%/../core/workflows/shaders -o %dir_path%/../core/workflows/spv -c %dir_path%/../dependences/libs/vulkan_tools/glslc.exe
+python3 %dir_path%/shaders_compile.py -d %dir_path%/../core/workflows/shaders -o %dir_path%/../core/workflows/spv -c %dir_path%/../dependences/vulkan_tools/glslc.exe
 
-python3 %dir_path%/shaders_compile.py -d %dir_path%/../core/rayTracingGraphics/shaders -o %dir_path%/../core/rayTracingGraphics/spv -c %dir_path%/../dependences/libs/vulkan_tools/glslc.exe
+python3 %dir_path%/shaders_compile.py -d %dir_path%/../core/rayTracingGraphics/shaders -o %dir_path%/../core/rayTracingGraphics/spv -c %dir_path%/../dependences/vulkan_tools/glslc.exe
